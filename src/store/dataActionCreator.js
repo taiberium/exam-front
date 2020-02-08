89510c5d6c7a24f781de6cebeaf7ee57880c786e
taiberium <taiberium@mail.ru>
2020-02-08 16:54:18 +0300

    change api

--- src/store/dataActionCreator.js
+++ src/store/dataActionCreator.js
@@ -2,7 +2,7 @@ import {createAsyncAction} from "redux-promise-middleware-actions";
 import request from 'superagent';
 
 export const sendDataActionCreator = createAsyncAction("DATA", (numberString) => {
-    return request.post("/data")
+    return request.post("/api/data")
         .send({numbers: numberString})
         .then(response => response.body)
         .catch(error => Promise.reject(error.response.body));
